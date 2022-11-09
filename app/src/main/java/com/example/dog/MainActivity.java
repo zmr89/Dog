c838eecf331d2649e6be1d57263371c73e038380
zmr89 <marsell.ru@gmail.com>
2022-11-09 15:40:39 +0500

    Implementation ViewModel and RxJava

--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -17,57 +19,27 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    MainViewModel mainViewModel;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadDogImage();
-    }
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-    private void loadDogImage(){
-        Thread thread = new Thread(new Runnable() {
+        mainViewModel.loadDogImage();
+
+        mainViewModel.getDogImageMLD().observe(this, new Observer<DogImage>() {
             @Override
-            public void run() {
-
-                try {
-//Создаем URL для работы сос сылкой
-                    URL loadURL = new URL(BASE_URL);
-//Открываем подключение с помощью метода openConnection()
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) loadURL.openConnection();
-//Считываем поток побайтово с помощью InputStream
-                    InputStream inputStream = httpURLConnection.getInputStream();
-//Преобразуем поток байтов в поток символов с помощью InputStreamReader
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-//тобы не читать каждый символ по отдельности, а сразу всю строку целиком испоьзуем BufferedReader
-                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                    StringBuilder data = new StringBuilder();
-                    String result;
-
-                    do {
-                        result = bufferedReader.readLine();
-                        data.append(result);
-                    } while (result != null);
-
-                    JSONObject jsonObject = new JSONObject(data.toString());
-                    String message = jsonObject.getString("message");
-                    String status = jsonObject.getString("status");
-                    DogImage dogImage = new DogImage(message, status);
-
-
-                    Log.d("MainActivity.java", dogImage.toString());
-
-                } catch (Exception e) {
-                    Log.d("MainActivity.java",e.getMessage());
-                }
-
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
             }
         });
 
-        thread.start();
+
     }
+
+
 }
